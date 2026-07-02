2418af3e8cf0b9ce579565846baa751ff306cb3e
isasera30 <isasera30@gmail.com>
2026-07-02 21:30:15 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE="gyesirok-memory-v5-30-exam-ui-blank-level";
+const CACHE="gyesirok-memory-v5-38-hint-options-fix";
 const ASSETS=["./","./index.html","./style.css","./app.js","./manifest.json","./icon-192.png","./icon-512.png"];
 
 self.addEventListener("install", event => {
