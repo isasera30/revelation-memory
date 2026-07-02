3fcb1bbf225d96298c66fdcf5bf40e5af32af3f1
isasera30 <isasera30@gmail.com>
2026-07-02 16:01:26 +0900

    Add files via upload

--- app.js
+++ app.js
@@ -2300,7 +2300,7 @@ function resetStudyDataOnly(){
 }
 
 
-const APP_VERSION="5.20-mobile-reset-layout";
+const APP_VERSION="5.20-mobile-reset-actual";
 
 function formatDateTime(ts){
   if(!ts)return "";
