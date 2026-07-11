8225096019efa3cd0c7d6027e19314e481d5a071
isasera30 <isasera30@gmail.com>
2026-07-12 02:08:32 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE="gyesirok-memory-v5-68-exam-char-diff";
+const CACHE="gyesirok-memory-v5-70-desktop-safari-form-fix";
 const ASSETS=["./","./index.html","./style.css","./app.js","./manifest.json","./icon-192.png","./icon-512.png"];
 
 self.addEventListener("install", event => {
